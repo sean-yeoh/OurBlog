e0f8a24bdeac4c3760b50fd1cd7859cc8a1e772e
Sean Yeoh <sean.yeoh@hotmail.com>
2017-01-05 23:31:46 +0800

    Added posts controller
    Added views/posts/new and form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,19 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -18,5 +26,10 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 20170105144432) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
-    t.text     "body"
+    t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
