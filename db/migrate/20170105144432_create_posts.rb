0e7de7dccf2b718f55aae704ea5dd05c8efba698
Sean Yeoh <sean.yeoh@hotmail.com>
2017-01-10 20:58:29 +0800

    Added associations for user and post model

--- db/migrate/20170105144432_create_posts.rb
+++ db/migrate/20170105144432_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[5.0]
     create_table :posts do |t|
       t.string :title
       t.text :content
-
+      t.references :user, foreign_key: true
       t.timestamps null: false
     end
   end
