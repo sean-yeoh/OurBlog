a03d04313976226aa22d9f3460f4886338b06995
Sean Yeoh <sean.yeoh@hotmail.com>
2017-01-12 10:40:51 +0800

    Added helper method for adding bootstrap alert to error message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # For displaying flash message using bootstrap
+  def flash_class(level)
+    case level.to_sym
+      when :notice then "alert alert-danger"
+      when :info then "alert alert-info"
+      when :alert then "alert alert-danger"
+      when :warning then "alert alert-warning"
+    end
+  end
 end
