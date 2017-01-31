5349fe01684c4f58076f8d3632eee5b00e5b9217
Sean Yeoh <sean.yeoh@hotmail.com>
2017-01-31 12:31:32 +0800

    Fix a bug with album#destroy because i did not remove has_many uploads in album.rb

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,6 +2,5 @@ class Album < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   belongs_to :user
   has_many :photos, dependent: :destroy
-  has_many :uploads, dependent: :destroy
   default_scope  { order(:created_at => :desc) }
 end
