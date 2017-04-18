5118496fcf3c95494e08307ba8bec792279c2be7
Sean Yeoh <sean.yeoh@hotmail.com>
2017-04-18 21:56:26 +0800

    Change /photos_all to /photos

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'posts#index'
   get '/cancel_new', to: 'albums#cancel_new'
   get '/cancel_edit', to: 'albums#cancel_edit'
-  get '/photos_all', to: 'photos#index'
+  get '/photos', to: 'photos#index'
   get '/albums/:id/edit_album', to: 'albums#edit_album_with_photos', as: 'edit_album_with_photos'
   delete '/albums/:id/delete_selected', to: "photos#delete_selected", as: 'delete_selected'
   delete '/albums/:id/delete_all', to: "photos#delete_all", as: 'delete_all'
